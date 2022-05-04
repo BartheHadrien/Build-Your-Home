9369af3e843d3eb6410def23bc3ea0bfb47a620c
BartheHadrien <hadrien.barthe@hotmail.fr>
2022-05-04 14:48:28 +0200

    feature-Contact OK

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,27 +7,56 @@ function Contacts() {
         <h1 className="contact--left__title"> Une question, un besoin, un projet ?</h1>
         <h2 className="contact--left__subtitle"> Contactez-nous, nos conseillers vous répondent !</h2>
         <div className="contact--left__container">
-          <h1 className="contact--left__container__title">Damien Savary</h1>
-          <p className="contact--left__container__text">02 98 02 02 02 </p>
+          <div className="contact--left__card">
+            <h1 className="contact--left__card__title">Prénom Nom</h1>
+            <p className="contact--left__card__text">
+              02 98 02 02 02
+              <br />
+              Rue de la paix
+              <br />
+              75000 Paris
+            </p>
+          </div>
+          <div className="contact--left__card">
+            <h1 className="contact--left__card__title">Prénom Nom</h1>
+            <p className="contact--left__card__text">
+              02 98 02 02 02
+              <br />
+              Rue de la paix
+              <br />
+              75000 Paris
+            </p>
+          </div>
         </div>
+
       </section>
       <section className="contact--right">
         <div className="contact--right--container">
           <h1 className="contact--right--container__title">Demande d'information</h1>
           <form className="contact--right--container__form">
             <input
-              className="login--field__input"
-              type="email"
-              id="email"
+              className="contact--right--container__form__input"
+              type="text"
+              placeholder="Nom et Prénom"
             />
             <input
-              className="login--field__input"
-              type="password"
-              id="password"
+              className="contact--right--container__form__input"
+              type="text"
+              placeholder="Email"
+            />
+            <input
+              className="contact--right--container__form__input"
+              type="text"
+              placeholder="Téléphone"
+            />
+            <textarea
+              className="contact--right--container__form__input"
+              type="text"
+              placeholder="Commentaire"
             />
             <button
               type="submit"
-              className="login--button__submit"
+              className="contact--right--container__form__submit"
             >
               Envoyer ma demande
             </button>
