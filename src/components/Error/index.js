959e4c10ff888825f585d703aceabfde79b88f8a
martin-mickael <mickaelmartin29.pro@gmail.com>
2022-05-09 10:18:11 +0200

    features- add error page and link to home (pair prog with Hadrien)

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,10 +1,12 @@
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
     <div className="error">
       <h1>Error 404</h1>
-      <p> </p>
+      <p>Cette page n'existe pas.</p>
+      <p>Reformulez votre recherche ou allez sur <Link to="/">la page d'acceuil de Build Your Home</Link>.</p>
     </div>
   );
 }
